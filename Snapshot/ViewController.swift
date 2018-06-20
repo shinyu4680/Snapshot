f2c1859ee7deb63260a8fbc7b3148cbe9749c1d4
張心禹 <kevin@kevins-macbook-pro.local>
2018-06-20 20:15:56 +0800

    Initial Commit

---
+++ Snapshot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Snapshot
+//
+//  Created by kevin on 2018/6/20.
+//  Copyright © 2018 KevinChang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
